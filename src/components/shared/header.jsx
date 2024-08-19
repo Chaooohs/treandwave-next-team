0e1038652af07a4fdbbd174c640d9398e62b7cd8
Chaooohs <01nikka01@gmail.com>
2024-08-19 18:19:24 +0300

    asdf

--- src/components/shared/header.jsx
+++ src/components/shared/header.jsx
@@ -1,5 +1,6 @@
 'use client'
 import { usePathname } from "next/navigation"
+import { cn } from "@/lib/utils"
 
 import Link from "next/link"
 import { Button } from "../ui"
@@ -7,22 +8,43 @@ import { Button } from "../ui"
 
 export const Header = () => {
   const pathname = usePathname()
-  console.log(pathname)
+
   return (
     <header>
       <div className="flex align-center h-20 border-b items-center gap-x-8 px-8 box-border">
-        <Link href='/' className={`${pathname === '/' && 'bg-red-500 text-white'}`}>
-          <Button variant='outline' size='lg'>
+        <Link href='/'>
+          <Button
+            variant='default'
+            size='lg'
+            className={`
+              bg-blue-500 hover:bg-blue-400  rounded text-white 
+              ${pathname === '/' && 'bg-blue-400'}
+              `}
+          >
             main
           </Button>
         </Link>
-        <Link href='/pageone' className={`${pathname === '/pageone' && 'bg-red-500 text-white'}`}>
-          <Button variant='outline' size='lg'>
+        <Link href='/pageone' >
+          <Button
+            variant='default'
+            size='lg'
+            className={`
+              bg-blue-500 hover:bg-blue-400  rounded text-white 
+              ${pathname === '/pageone' && 'bg-blue-400'}
+              `}
+          >
             pageone
           </Button>
         </Link>
-        <Link href='/pagetwo' className={`${pathname === '/pagetwo' && 'bg-red-500 text-white'}`}>
-          <Button variant='outline' size='lg'>
+        <Link href='/pagetwo'>
+          <Button
+            variant='default'
+            size='lg'
+            className={`
+            bg-blue-500 hover:bg-blue-400  rounded text-white 
+            ${pathname === '/pagetwo' && 'bg-blue-400'}
+            `}
+          >
             pagetwo
           </Button>
         </Link>

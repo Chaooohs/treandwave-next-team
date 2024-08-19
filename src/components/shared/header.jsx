86bad1d3e2474c5aeb6c7cc2df0a190cb0e97ac5
Chaooohs <01nikka01@gmail.com>
2024-08-19 19:50:25 +0300

    asdf

--- src/components/shared/header.jsx
+++ src/components/shared/header.jsx
@@ -1,9 +1,7 @@
 'use client'
 import { usePathname } from "next/navigation"
-import { cn } from "@/lib/utils"
 
 import Link from "next/link"
-import { Button } from "../ui"
 
 
 export const Header = () => {
@@ -12,33 +10,22 @@ export const Header = () => {
   return (
     <header>
       <div className="flex align-center h-20 border-b items-center gap-x-8 px-8 box-border">
-        <Link href='/'>
-          <Button
-            variant='default'
-            size='lg'
-            className={`${pathname === '/' && 'text-[#D8001A]'}`}
-          >
-            main
-          </Button>
+
+        <Link href='/'
+          className={`${pathname === '/' && 'text-[#D8001A]'}`}
+        >
         </Link>
-        <Link href='/pageone' >
-          <Button
-            variant='default'
-            size='lg'
-            className={`${pathname === '/pageone' && 'text-[#D8001A]'}`}
-          >
-            pageone
-          </Button>
+
+        <Link href='/pageone'
+          className={`${pathname === '/pageone' && 'text-[#D8001A]'}`}
+        >
         </Link>
-        <Link href='/pagetwo'>
-          <Button
-            variant='default'
-            size='lg'
-            className={`${pathname === '/pagetwo' && 'text-[#D8001A]'}`}
-          >
-            pagetwo
-          </Button>
+
+        <Link href='/pagetwo'
+          className={`${pathname === '/pagetwo' && 'text-[#D8001A]'}`}
+        >
         </Link>
+
       </div>
     </header>
   )

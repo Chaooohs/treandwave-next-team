1d76e0243c9786b868cc155af98c6be749dce886
Anastasia <kop.anastasia27@gmail.com>
2024-08-23 14:18:14 +0300

    runningLine block, added to the Layout

---
+++ src/components/shared/runningLine.jsx
@@ -0,0 +1,32 @@
+'use client';
+import { motion } from "framer-motion";
+
+export default function RunningLine() {
+    const runningText = [
+        'Безкоштовна доставка від 2500 UAH',
+        'Безкоштовна доставка від 2500 UAH',
+        'Безкоштовна доставка від 2500 UAH',
+        'Безкоштовна доставка від 2500 UAH',
+        'Безкоштовна доставка від 2500 UAH',
+        'Безкоштовна доставка від 2500 UAH',
+        'Безкоштовна доставка від 2500 UAH',
+    ]
+    return(
+        <div className="sticky  top-0 z-50 h-12 bg-[#121212] max-w-screen overflow-hidden flex">
+            <motion.div 
+                animate={{ x: ['2%', '-48%'] }}
+                transition={{
+                    duration:25,
+                    repeat:Infinity,
+                    ease:'linear'
+                }}
+                className="flex gap-5 text-nowrap items-center">
+                {runningText.map((item, index) => (
+                    <p key={index} className="font-mont text-[#FDFDFD] font-medium text-base">
+                        {item}
+                    </p>
+                ))}
+            </motion.div>
+        </div>
+    )
+}

77abc385ffbdc69860663d37f9111a4c4a89f1d8
Chaooohs <01nikka01@gmail.com>
2024-08-23 20:28:13 +0300

    sdf

--- src/components/shared/header.jsx
+++ src/components/shared/header.jsx
@@ -6,6 +6,8 @@ import { openBurger } from "@/redux/features/openSlice"
 import Search from '../../../public/image/svg/search.svg'
 import Heart from '../../../public/image/svg/heart.svg'
 import Cart from '../../../public/image/svg/cart.svg'
+import Man from '../../../public/image/svg/man.svg'
+import Logo from '../../../public/image/svg/logo.svg'
 import ArrowDown from '../../../public/image/svg/arrow-down.svg'
 import { BurgerMenu } from "."
 
@@ -16,8 +18,8 @@ export const Header = () => {
 
 
   return (
-    <header className="relative">
-      <div className="px-[3cqi]">
+    <header className="sticky z-40 top-12 bg-[#fdfdfd]">
+      <div className="wrap">
         <div className="flex align-center justify-between items-center h-20 gap-x-8 text-header">
 
           <nav className="flex items-center gap-x-6">
@@ -43,19 +45,21 @@ export const Header = () => {
           </nav>
 
           <Link href='/'>
-            <div className="font-mont font-semibold uppercase text-2xl">Logo</div>
+            <Logo/>
           </Link>
 
           <nav className="flex gap-x-6">
 
+            <Link href='#!' className="header-link transit">
+              <Man className='header-icon' />
+            </Link>
+
             <Link href='/search' className="header-link transit">
               <Search className='header-icon' />
-              <span className="transit">Пошук</span>
             </Link>
 
             <Link href='/wishlist' className="header-link">
               <Heart className='header-icon' />
-              <span >Вішліст</span>
             </Link>
 
             <Link href='/cart' className="header-link">

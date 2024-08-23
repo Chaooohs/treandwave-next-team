055bc334b51d3ca6968341103a6f35fe5709efd7
Chaooohs <01nikka01@gmail.com>
2024-08-23 15:26:34 +0300

    sdf

--- src/app/wishlist/page.jsx
+++ src/app/wishlist/page.jsx
@@ -11,49 +11,50 @@ export default function WishList() {
 
   return (
     <main>
-      {
-        wishlist.length > 0
-          ?
-          <div className="wrap">
-            <div className="content">
-              <div className="text-center">
-                <Title text={'ваш вішліст'} size="xl" className='font-mul font-extrabold uppercase' />
-                <span className="font-medium text-sm">Збережені товари чекають на вас!</span>
+      <div className="content pt-6">
+        {
+          wishlist.length > 0
+            ?
+            <div className="wrap">
+              {/* <div className="content"> */}
+                <div className="text-center">
+                  <Title text={'ваш вішліст'} size="xl" className='font-mul font-extrabold uppercase' />
+                  <span className="font-medium text-sm">Збережені товари чекають на вас!</span>
+                </div>
+                <div className="card-layout">
+                  {
+                    Array.isArray(wishlist) &&
+                    wishlist.map((el) => {
+                      return (
+                        <div key={el.id} className="relative">
+                          <Card el={el} />
+                        </div>
+                      )
+                    })
+                  }
+                </div>
               </div>
-              <div className="card-layout">
-                {
-                  Array.isArray(wishlist) &&
-                  wishlist.map((el) => {
-                    return (
-                      <div key={el.id} className="relative">
-                        <Card el={el} />
-                      </div>
-                    )
-                  })
-                }
+            // </div>
+            :
+            <div className="h-full flex items-center justify-center">
+              <div className="w-[612px] flex flex-col items-center text-center">
+                <Heart />
+                <Title text={'ваш вішліст порожній'} size="xs" className='font-semibold uppercase mt-3' />
+                <span className="font-medium text-sm mt-1">
+                  Додавайте товари до свого вішлісту. Просто натисніть на іконку серця поруч із товаром, і він з&#39;явиться тут.
+                </span>
+                <Link href='/'>
+                  <Button
+                    variant='outline'
+                    className='w-[130px] h-11 mt-6'
+                  >
+                    Каталог
+                  </Button>
+                </Link>
               </div>
             </div>
-          </div>
-          :
-          <div className="h-full flex items-center justify-center">
-            <div className="w-[612px] flex flex-col items-center text-center">
-              <Heart />
-              <Title text={'ваш вішліст порожній'} size="xs" className='font-semibold uppercase mt-3' />
-              <span className="font-medium text-sm mt-1">
-                Додавайте товари до свого вішлісту. Просто натисніть на іконку серця поруч із товаром, і він з&#39;явиться тут.
-              </span>
-              <Link href='/'>
-                <Button
-                  variant='outline'
-                  className='w-[130px] h-11 mt-6'
-                >
-                  Каталог
-                </Button>
-              </Link>
-            </div>
-          </div>
-      }
-
+        }
+      </div>
     </main>
   )
 }

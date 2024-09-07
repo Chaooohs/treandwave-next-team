8396ca2baadf133b04f30291a5984e8cc37bef27
Chaooohs <01nikka01@gmail.com>
2024-09-07 14:22:01 +0300

    adsf

--- src/components/shared/you.jsx
+++ src/components/shared/you.jsx
@@ -1,22 +1,28 @@
-import { Title } from "../ui"
+import { CardYou, Title } from "../ui"
 import Heart from '../../../public/image/svg/heart2.svg'
-import SwiperYou from "../ui/swiper-you"
 
 
 export const YouSection = () => {
   return (
     <section>
-      <div className="wrap">
+      <div className="wrap mob:px-4">
         <div className="content">
 
           <div className="flex items-center gap-x-3">
-            <Title text='Ви' size='xl' className='font-mul font-extrabold uppercase lg:text-3xl mob:text-2xl' />
+            <Title text='Ви' size='xl' className='font-mul font-extrabold uppercase lap:text-3xl mob:text-2xl' />
             <Heart className='w-48' />
           </div>
 
         </div>
+      </div >
+      <div className="lap:h-[450px] lap:relative lap:h-[396px] mob:h-[222px]">
+        <div className="lap:flex lap:overflow-auto lap:absolute lap:inset-0 lap:pl-6 mob:pl-4 gap-x-6">
+          <div className="relative"><CardYou/></div>
+          <div className="relative"><CardYou/></div>
+          <div className="relative"><CardYou/></div>
+          <div className="relative"><CardYou/></div>
+        </div>
       </div>
-      <SwiperYou />
     </section>
   )
 }
--- src/components/ui/card-you.jsx
+++ src/components/ui/card-you.jsx
@@ -8,12 +8,12 @@ import Dots from '../../../public/image/svg/dots.svg'
 
 export const CardYou = () => {
   return (
-    <div className="w-full border border-gray-100 rounded grid grid-rows-[auto 1fr auto]">
+    <div className="w-full lap:w-[340px] mob:w-[182px]">
       <header>
-        <div className="p-4 box-border flex items-center justify-between">
+        <div className="p-4 box-border flex items-center justify-between lap:p-3 mob:p-2">
           <div className="flex items-center gap-x-2">
-            <Image src={avatar} alt="avatar" width={32} height={32} className="w-8 h-8 rounded-full"/>
-            <Title text='laura_perez' className='font-mont text-sm font-semibold leading-5'/>
+            <Image src={avatar} alt="avatar" width={32} height={32} className="w-8 h-8 rounded-full lap:w-8 lap:h-68 mob:w-6 mob:h-6"/>
+            <Title text='laura_perez' className='font-mont text-sm font-semibold leading-5 mob:text-xs'/>
           </div>
           <button>
             <Dots width={24} height={24}/>
@@ -21,13 +21,8 @@ export const CardYou = () => {
         </div>
       </header>
       <main>
-        <Image src={laura} alt="img" width={340} height={340} className="w-full"/>
+        <Image src={laura} alt="img" width={340} height={340} className="w-full lap:w-[340px] mob:w-[182px]"/>
       </main>
-      <footer>
-        <div className="p-4">
-          <span className="font-mont text-sm font-medium">sunbathing ☀️</span>
-        </div>
-      </footer>
     </div>
   )
 }

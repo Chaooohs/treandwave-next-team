c90b8cb70f036cdd684936f5843277b5b0920900
Chaooohs <01nikka01@gmail.com>
2024-09-08 02:57:20 +0300

    sdf

--- src/components/shared/header/Header.jsx
+++ src/components/shared/header/Header.jsx
@@ -1,23 +1,42 @@
 'use client'
-import { useMediaQuery } from "react-responsive"
+import { createMedia } from "@artsy/fresnel"
+// import { useMediaQuery } from "react-responsive"
 import { HeaderDesk } from "./HeaderDesk"
 import { HeaderMobile } from "./HeaderMobile"
 
 
 export const Header = () => {
-  const isDesktop = useMediaQuery({ minWidth: 1024 })
-  const isMobile = useMediaQuery({ maxWidth: 1023 })
+  // const isDesktop = useMediaQuery({ minWidth: 1024 })
+  // const isMobile = useMediaQuery({ maxWidth: 1023 })
+
+
+  const { MediaContextProvider, Media } = createMedia({
+    breakpoints: {
+      md: 0,
+      lg: 1023,
+    },
+  })
 
   return (
     <>
-      {
+      {/* {
         isDesktop &&
         <HeaderDesk />
       }
       {
         isMobile &&
         <HeaderMobile />
-      }
+      } */}
+
+      <MediaContextProvider>
+        <Media at="md">
+          <HeaderMobile />
+        </Media>
+        <Media at="lg">
+          <HeaderDesk />
+        </Media>
+      </MediaContextProvider>
+
     </>
   )
 }

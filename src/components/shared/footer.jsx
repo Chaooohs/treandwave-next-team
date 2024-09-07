9da123d2f87cba3089696ca81a3193d1d0098f94
Chaooohs <01nikka01@gmail.com>
2024-09-08 02:17:05 +0300

    asdf

--- src/components/shared/footer.jsx
+++ src/components/shared/footer.jsx
@@ -60,27 +60,27 @@ export const Footer = () => {
   return (
     <footer>
       <div className="font-mont bg-[#121212] text-white">
-        <div className="px-24 md:px-5 lg:px-10 grid grid-cols-2 md:grid-cols-1 gap-20 lg:gap-5 py-20">
-          <div className="flex flex-col gap-5 justify-center">
+        <div className="px-24 mob:px-5 lap:px-10 grid grid-cols-[1fr,_3fr] lap:grid-cols-[1fr,_2fr] mob:grid-cols-1 gap-20 lap:gap-5 py-20 mob:py-10">
+          <div className="flex flex-col gap-6 justify-center">
             <div>
               <Logo fill='white' stroke='white' className={''} />
             </div>
             <div className="flex gap-3">
               <Link href='https://uk-ua.facebook.com/'>
-                <Fb fill="#FDFDFD"/>
+                <Fb fill="#FDFDFD" />
               </Link>
               <Link href='https://www.instagram.com/'>
-                <Inst fill="#FDFDFD"/>
+                <Inst fill="#FDFDFD" />
               </Link>
               <Link href='https://web.telegram.org/'>
-                <Telegram fill="#FDFDFD"/>
+                <Telegram fill="#FDFDFD" />
               </Link>
               <Link href='https://www.tiktok.com/uk-UA/'>
-                <TikTok fill="#FDFDFD"/>
+                <TikTok fill="#FDFDFD" />
               </Link>
             </div>
           </div>
-          <div className="flex gap-20 md:gap-5 justify-start md:justify-between">
+          <div className="flex gap-20 mob:gap-5 justify-start mob:justify-between mob:mt-16 mob:row-start-2 mob:row-end-3 mob:col-start-1 col-end-3">
             {footerLinks.map((item, index) => (
               <div key={index} className="flex flex-col gap-5">
                 <Title2 text={item.title} />
@@ -89,11 +89,11 @@ export const Footer = () => {
                     <div key={index}>
                       {item.subtitle.toLocaleLowerCase() === 'sale' ? (
                         <Link href={item.href}>
-                          <Button variant="buttonRed" size="base" className=' rounded-none py-1'>{item.subtitle}</Button>
+                          <Button variant="buttonRed" size="base" className=' rounded-none py-1 mob:text-xs'>{item.subtitle}</Button>
                         </Link>
                       ) : (
                         <Link href={item.href}>
-                          <Button variant="button2" size="base" className='rounded-none py-1'>{item.subtitle}</Button>
+                          <Button variant="button2" size="base" className='rounded-none py-1 mob:text-xs'>{item.subtitle}</Button>
                         </Link>
                       )}
                     </div>
@@ -102,16 +102,12 @@ export const Footer = () => {
               </div>
             ))}
           </div>
-          <div>
-            <p className="font-medium text-sm">
-              © {currentYear} TreandWave
-            </p>
-          </div>
-          <div>
-            <p className="font-medium text-sm">
-              Політика конфіденційності
-            </p>
-          </div>
+          <span className="font-medium text-sm lap:text-xs mt-8 lap:mt-[28px] mob:mt-6 mob:row-start-3 mob:row-end-4">
+            © {currentYear} TreandWave
+          </span>
+          <span className="font-medium text-sm lap:text-xs mt-8 lap:mt-[28px] mob:mt-6 mob:row-start-3 mob:row-end-4">
+            Політика конфіденційності
+          </span>
         </div>
       </div>
     </footer>
--- src/components/ui/title2.jsx
+++ src/components/ui/title2.jsx
@@ -1,6 +1,6 @@
 export default function Title2({text}) {
     return(
-        <div className="text-[#FDFDFD] font-mont font-semibold text-2xl uppercase">
+        <div className="text-[#FDFDFD] font-mont font-semibold text-2xl uppercase lap:text-lg">
             <h2>{text}</h2>
         </div>
     )

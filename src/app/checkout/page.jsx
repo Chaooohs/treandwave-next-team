b7c06daffc3cac04f2a58ebe782af9af91eb92fd
Chaooohs <01nikka01@gmail.com>
2024-09-09 21:32:04 +0300

    aside

--- src/app/checkout/page.jsx
+++ src/app/checkout/page.jsx
@@ -1,8 +1,12 @@
 'use client'
 import { useSelector } from "react-redux";
+import Link from "next/link";
 
-import { Title } from "@/components/ui";
+import { Button, Title } from "@/components/ui";
 import { CardForCart } from "@/components/shared";
+import VisaIcon from '/public/image/svg/visa-logo.svg'
+import MasterIcon from '/public/image/svg/mastercard-logo.svg'
+import AppleIcon from '/public/image/svg/applepay-logo.svg'
 
 
 export default function ShoppingCart() {
@@ -33,7 +37,34 @@ export default function ShoppingCart() {
                 })}
             </main>
             <aside>
-
+              <div className="border rounded border-[#EDEDED] p-6 flex flex-col gap-6">
+                <h3 className="uppercase font-semibold text-2xl">Ваше замовлення</h3>
+                <div className="flex justify-between items-center">
+                  <span className="text-base">Товарів на суму</span>
+                  <span className="text-lg font-medium uppercase"> uah</span>
+                </div>
+                <div className="flex justify-between items-center uppercase">
+                  <span className="font-semibold text-base uppercase">До сплати</span>
+                  <span className="text-2xl font-semibold uppercase"> uah</span>
+                </div>
+              </div>
+              <Button
+                className='font-semibold text-base uppercase h-14 w-full mt-10'
+              >
+                Оформити замовлення
+              </Button>
+              <div className="mt-16 text-lg font-semibold uppercase">Потрібна допомога?</div>
+              <div className="mt-3 flex flex-col text-base font-medium gap-y-2 underline">
+                <Link href='#!'>Оплата і доставка</Link>
+                <Link href='#!'>Обмін та повернення</Link>
+                <Link href='#!'>Контакти</Link>
+              </div>
+              <div className="mt-10 text-lg font-semibold uppercase">Методи оплати</div>
+              <div className="flex gap-x-6 items-center mt-6">
+                <VisaIcon/>
+                <MasterIcon/>
+                <AppleIcon/>
+              </div>
             </aside>
           </div>
         </div>

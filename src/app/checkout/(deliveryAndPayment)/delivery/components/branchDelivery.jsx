58c7254f0967c5d733bda672ad271b083bab4a51
Anastasia <kop.anastasia27@gmail.com>
2024-09-16 15:27:35 +0300

    added addresses

---
+++ src/app/checkout/(deliveryAndPayment)/delivery/components/branchDelivery.jsx
@@ -0,0 +1,152 @@
+import * as React from "react"
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import { useState, useEffect } from 'react';
+
+
+export function BranchDelivery({setDeliveryInfo}) {
+
+  const [search, setSearch] = useState('');
+  const [cities, setCities] = useState([]);
+  const [devisions, setDevisions] = useState([]);
+
+  const [selectedCity, setSelectedCity] = useState('');
+  const [selectedDivision, setSelectedDivision] = useState('')
+  const [isDropDownOpen, setIsDropDownOpen] = useState(false);
+
+  useEffect(() => {
+    const deliveryInfo = {
+      selectedCity,
+      selectedDivision,
+    };
+    setDeliveryInfo(deliveryInfo);
+  }, [selectedCity, selectedDivision]);
+
+  const handleInputChange = (e) => {
+      setSearch(e.target.value);
+  };
+
+  const handleFocus = () => {
+    setIsDropDownOpen(true);
+  };
+
+  const handleSelectedAddress = (address) => {
+    setSearch(address);
+    setSelectedCity(address);
+    setIsDropDownOpen(false);
+  }
+
+  useEffect(() => {
+    fetchCities().then(setCities);
+}, []);
+
+  useEffect(() => {
+    if (search.length >= 1) {
+      fetchCities(search).then(setCities);
+    } else {fetchCities('').then(setCities);}
+  }, [search]);
+
+  useEffect(() => {
+    if (selectedCity) {
+      fetchDevision(selectedCity).then(setDevisions);
+    }
+  }, [selectedCity]);
+
+  return (
+    <div className="w-full">
+      <div className="w-full flex flex-col gap-10">
+        <div className="bg-[#EDEDED] h-[1px] w-full"></div>
+
+        {/*  address  */}
+        <div className="flex flex-col gap-5 w-full">
+          <h3 className="uppercase font-semibold text-base">Адреса відділення</h3>
+          <div>
+            <input 
+              type="text" 
+              value={search}
+              placeholder="Місто"
+              onChange={handleInputChange}
+              onFocus={handleFocus}
+              className={`w-full p-2 font-mont border-[1px] text-black rounded-sm border-[#BABABA] font-medium  `}
+              />
+              {isDropDownOpen && 
+                <div className="border-[1px] pb-4 ">
+                  <div className=" flex flex-col gap-2 max-h-96 overflow-auto p-2 pt-4 ">
+                    {cities.map((item, index) => (
+                      <div key={index} onClick={() => handleSelectedAddress(item.Description)}
+                      className=" cursor-pointer">
+                        {item.Description}
+                      </div>
+                    ))}
+                  </div>
+                </div>
+            }
+          </div>
+          <div>
+            <Select onValueChange={setSelectedDivision}>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="Відділення" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Відділення</SelectLabel>
+                  {devisions.map((item, index) => (
+                    <SelectItem key={index} value={item.Description}>{item.Description}</SelectItem>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+  )
+}
+
+async function fetchCities(search) {
+  const res = await fetch('https://api.novaposhta.ua/v2.0/json/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify({
+              "apiKey": "692fdef2615463a4b951f6bb0754ec97",
+              "modelName": "AddressGeneral",
+              "calledMethod": "getCities",
+              "methodProperties": {
+                  "FindByString" : search || '',
+                  "Limit" : "1000"
+              }
+                  })
+  });
+
+  const data = await res.json();
+  return data.data;
+}
+
+async function fetchDevision(params) {
+  const res = await fetch('https://api.novaposhta.ua/v2.0/json/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify({
+              "apiKey": "692fdef2615463a4b951f6bb0754ec97",
+              "modelName": "AddressGeneral",
+              "calledMethod": "getWarehouses",
+              "methodProperties": {
+                  "CityName" : params,
+              }
+                  })
+  });
+
+  const data = await res.json();
+  const filteredData = data.data.filter(item => item.CategoryOfWarehouse !== 'Postomat')
+  return filteredData;
+}
---
+++ src/app/checkout/(deliveryAndPayment)/delivery/components/courierDelivery.jsx
@@ -0,0 +1,169 @@
+import * as React from "react"
+import { useState, useEffect } from 'react';
+
+
+export function CourierDelivery({setDeliveryInfo}) {
+
+  const [search, setSearch] = useState('');
+  const [cities, setCities] = useState([]);
+  const [searchStreet, setSearchStreet] = useState('');
+  const [streets, setStreets] = useState([]);
+
+  const [selectedCityRef, setSelectedCityRef] = useState('');
+  const [selectedCity, setSelectedCity] = useState('');
+  const [selectedStreet, setSelectedStreet] = useState('')
+  const [isDropDownOpen, setIsDropDownOpen] = useState(false);
+  const [isStreetDropDownOpen, setStreetIsDropDownOpen] = useState(false);
+
+  const handleInputChange = (e) => {
+      setSearch(e.target.value);
+  };
+
+  const handleInputChangeStreet = (e) => {
+    setSearchStreet(e.target.value);
+};
+
+  const handleFocus = () => {
+    setIsDropDownOpen(true);
+  };
+
+  const handleFocusStreet = () => {
+    setStreetIsDropDownOpen(true);
+  };
+
+  const handleSelectedAddress = (address, ref) => {
+    setSearch(address);
+    setSelectedCity(address);
+    setSelectedCityRef(ref);
+    setIsDropDownOpen(false);
+  };
+
+  const handleSelectedStreet = (address) => {
+    setSearchStreet(address);
+    setSelectedStreet(address);
+    setStreetIsDropDownOpen(false);
+  }
+
+  useEffect(() => {
+    fetchCities().then(setCities);
+}, []);
+
+  useEffect(() => {
+    if (search.length >= 1) {
+      fetchCities(search).then(setCities);
+    } else {fetchCities('').then(setCities);}
+  }, [search]);
+
+  useEffect(() => {
+    if (selectedCityRef) {
+      fetchStreet(selectedCityRef, searchStreet).then(setStreets);
+    }
+  }, [selectedCityRef, searchStreet]);
+
+  useEffect(() => {
+    const deliveryInfo = {
+      selectedCity,
+      selectedStreet,
+    };
+    setDeliveryInfo(deliveryInfo);
+  }, [selectedCity, selectedStreet]);
+
+
+  return (
+    <div className="w-full">
+      <div className="w-full flex flex-col gap-10">
+        <div className="bg-[#EDEDED] h-[1px] w-full"></div>
+
+        {/*  address  */}
+        <div className="flex flex-col gap-5 w-full">
+          <h3 className="uppercase font-semibold text-base">Адреса</h3>
+          <div>
+            <input 
+              type="text" 
+              value={search}
+              placeholder="Місто"
+              onChange={handleInputChange}
+              onFocus={handleFocus}
+              className={`w-full p-2 font-mont border-[1px] text-black rounded-sm border-[#BABABA] font-medium  `}
+              />
+              {isDropDownOpen && 
+                <div className="border-[1px] pb-4 ">
+                  <div className=" flex flex-col gap-2 max-h-96 overflow-auto p-2 pt-4 ">
+                    {cities.map((item, index) => (
+                      <div key={index} onClick={() => handleSelectedAddress(item.Description, item.Ref)}
+                      className=" cursor-pointer">
+                        {item.Description}
+                      </div>
+                    ))}
+                  </div>
+                </div>
+            }
+          </div>
+          <div>
+            <div>
+                <input 
+                type="text" 
+                value={searchStreet}
+                placeholder="Вулиця"
+                onChange={handleInputChangeStreet}
+                onFocus={handleFocusStreet}
+                className={`w-full p-2 font-mont border-[1px] text-black rounded-sm border-[#BABABA] font-medium  `}
+                />
+                {isStreetDropDownOpen && 
+                    <div className="border-[1px] pb-4 ">
+                    <div className=" flex flex-col gap-2 max-h-96 overflow-auto p-2 pt-4 ">
+                        {streets.map((item, index) => (
+                        <div key={index} onClick={() => handleSelectedStreet(item.Description)}
+                        className=" cursor-pointer">
+                            {item.Description}
+                        </div>
+                        ))}
+                    </div>
+                    </div>
+                }
+            </div>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+  )
+}
+
+async function fetchCities(search) {
+  const res = await fetch('https://api.novaposhta.ua/v2.0/json/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify({
+              "apiKey": "692fdef2615463a4b951f6bb0754ec97",
+              "modelName": "AddressGeneral",
+              "calledMethod": "getCities",
+              "methodProperties": {
+                  "FindByString" : search || '',
+                  "Limit" : "1000"
+              }
+                  })
+  });
+
+  const data = await res.json();
+  return data.data;
+}
+
+async function fetchStreet(params, searchStreet) {
+  const res = await fetch('https://api.novaposhta.ua/v2.0/json/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify({
+              "apiKey": "692fdef2615463a4b951f6bb0754ec97",
+              "modelName": "AddressGeneral",
+              "calledMethod": "getStreet",
+              "methodProperties": {
+                  "CityRef" : params,
+                  "FindByString": searchStreet || '',
+              }
+                  })
+  });
+
+  const data = await res.json();
+  return data.data;
+}

1bedeadd54c58256c5ac1f84ff00b5109e0cd31d
Anastasia <kop.anastasia27@gmail.com>
2024-09-18 12:26:01 +0300

    added total from slice/redux

--- src/components/shared/checkoutInput.jsx
+++ src/components/shared/checkoutInput.jsx
@@ -4,21 +4,33 @@ import { Input } from '../ui/input.jsx';
 import { useState } from 'react';
 import { Tag } from 'lucide-react';
 
-export default function CheckoutInput({promocod}) {
+export default function CheckoutInput({setDiscount}) {
     const [promo, setPromo] = useState('');
     const [isValid, setIsValid] = useState(false);
     const [error, setError] = useState('');
+    const [appliedDiscount, setAppliedDiscount] = useState(0);
 
-    const validPromoCodes = ['DISCOUNT10', 'SALE20'];
+
+    const validPromoCodes = [
+        {promo: 'DISCOUNT10', discount: 10},
+        {promo: 'SALE20', discount: 20}
+    ];
 
     const handlePromo = () => {
-        if (validPromoCodes.includes(promo.trim().toUpperCase())) {
+        const foundPromo = validPromoCodes.find(
+            (item) => item.promo === promo.trim().toUpperCase()
+        );
+
+        if (foundPromo) {
             setIsValid(true);
-        } else {
-            setIsValid(false);
-            setError('Невірний промокод');
+            setError('');
+
+            setAppliedDiscount(foundPromo.discount);
+            setDiscount(foundPromo.discount);
         }
+
       };
+      console.log(appliedDiscount);
 
     return(
         <div className='flex h-10 text-sm w-full'>
@@ -26,7 +38,7 @@ export default function CheckoutInput({promocod}) {
                 <div>
                     <div className='bg-[#E2ECFF] rounded-[3px] p-5'>
                         <p className='text-[#0047FF]'>
-                            Промокод успішно застосовано! Знижка 500 UAH  врахована в підсумковій сумі.
+                            Промокод успішно застосовано! Знижка {appliedDiscount}%  врахована в підсумковій сумі.
                         </p>
 
                     </div>
@@ -39,7 +51,7 @@ export default function CheckoutInput({promocod}) {
                         placeholder={`Введіть промокод`}
                         value={promo}
                         onChange={(e) => setPromo(e.target.value)}
-                        className={` pl-10 font-mont border-[1px] border-r-0 rounded-none border-[#BABABA] font-medium  ${error ? 'border-[#C50018] text-[#C50018]' : 'border-stone-300 text-[#121212]'} hover:border-[#0047FF] focus:border-[#0047FF] focus:text-[#121212] focus-visible:ring-0`}
+                        className={` focus:outline-none focus:border-[1px] pl-10 font-mont border-[1px] border-r-0 rounded-none border-[#BABABA] font-medium  ${error ? 'border-[#C50018] text-[#C50018]' : 'border-stone-300 text-[#121212]'} hover:border-[#0047FF] focus:border-[#0047FF] focus:text-[#121212] focus-visible:ring-0`}
                     />
                     </div>
                     <div className='absolute z-10 rotate-90 pt-[1px] pl-5'>
